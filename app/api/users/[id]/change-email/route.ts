7269acdcab98e6c06660709e0d45fd8af089fa4c
mike14747 <mike4747@oh.rr.com>
2023-03-12 03:01:17 -0400

    Finished the change-email api route, but haven't tested it yet.

--- app/api/users/[id]/change-email/route.ts
+++ app/api/users/[id]/change-email/route.ts
@@ -1,3 +1,24 @@
-export async function POST() {
-    return new Response('Not working yet');
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+import { updateUserEmail } from '../../../../../lib/api';
+
+export async function PUT(req: NextRequest, { params }) {
+    // return new Response('Not working yet');
+    try {
+        const token = await getToken({ req });
+        if (!token) return NextResponse.json(null, { status: 401 });
+
+        const { email } = await req.json();
+        const id = params.id;
+
+        if (!id || !email) return NextResponse.json(null, { status: 400 });
+        if (token?.id !== id) return NextResponse.json(null, { status: 401 });
+
+        const response = await updateUserEmail(id, email);
+        if (!response) return NextResponse.json(null, { status: 500 });
+        return response?.changedRows === 1 ? NextResponse.json(null, { status: 200 }) : NextResponse.json(null, { status: 404 });
+    } catch (error) {
+        console.log('error:', error.message);
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
 }
