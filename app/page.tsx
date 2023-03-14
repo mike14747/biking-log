ec1bf69cab25376d9052ee7ca7acc709f8c7d591
mike14747 <mike4747@oh.rr.com>
2023-03-14 00:06:18 -0400

    Added an add-ride page and reworked the homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,39 @@
+import { getServerSession } from 'next-auth/next';
 import type { Metadata } from 'next';
-import AddRideData from './components/rideData/AddRideData';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
-    title: 'Biking Log - Homepage',
+    title: 'Biking Log - Home',
 };
 
-export default function Home() {
-    return (
-        <>
-            <main id="main">
-                <article>
-                    <p>
-                        This is the homepage in the app folder.
-                    </p>
-                </article>
+export default async function Home() {
+    const session = await getServerSession({
+        callbacks: { session: ({ token }) => token },
+    });
 
-                <AddRideData />
-            </main>
-        </>
+    return (
+        <main id="main">
+            {!session &&
+                <p>
+                    Since you are not logged in, display some component that describes what this app does... and prompts them to login or register.
+                </p>
+            }
+
+            {session &&
+                <article>
+                    <h2 className="page-heading">
+                        Dashboard
+                    </h2>
+
+                    <p>
+                        You are logged in, so this will be your dashboard.
+                    </p>
+
+                    <Link href="/add-ride">
+                        Add ride data
+                    </Link>
+                </article>
+            }
+        </main>
     );
 }
