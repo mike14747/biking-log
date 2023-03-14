91454b07a5ffc55f930555600ef8e781833dc056
mike14747 <mike4747@oh.rr.com>
2023-03-14 19:37:35 -0400

    Reworked the change-password api route.

--- app/api/users/[id]/change-password/route.ts
+++ app/api/users/[id]/change-password/route.ts
@@ -5,23 +5,22 @@ import { changePassword } from '../../../../../lib/api';
 export async function PUT(req: NextRequest, { params }) {
     try {
         const token = await getToken({ req });
-        if (!token) return NextResponse.json(null, { status: 401 });
 
         const { userId, password, resetPasswordToken } = await req.json();
         const id = params.id;
-
         if (!id || !password) return NextResponse.json(null, { status: 400 });
-        if ((!token && (!userId || !resetPasswordToken)) || token.id !== id) return NextResponse.json(null, { status: 401 });
 
-        let result =  { code: 400 };
-
-        if (userId && resetPasswordToken) result = await changePassword(parseInt(userId), password, resetPasswordToken);
-
-        if (token.id) {
-            result = await changePassword(parseInt(id), password);
+        if (token && token.id === id) {
+            const result = await changePassword(parseInt(id), password);
+            return result?.code ? NextResponse.json(null, { status: result.code }) : NextResponse.json(null, { status: 500 });
         }
 
-        return result?.code ? NextResponse.json(null, { status: result.code }) : NextResponse.json(null, { status: 500 });
+        if (userId && resetPasswordToken) {
+            const result = await changePassword(parseInt(userId), password, resetPasswordToken);
+            return result?.code ? NextResponse.json(null, { status: result.code }) : NextResponse.json(null, { status: 500 });
+        }
+
+        return NextResponse.json(null, { status: 400 });
     } catch (error) {
         console.log('error:', error.message);
         return NextResponse.json({ error: error.message }, { status: 500 });
